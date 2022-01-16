ff03f245ecc2a1a3af4fccd87882fc8e0240ac2b
Nok <dive877@gmail.com>
2022-01-16 16:00:47 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Tesseract from "tesseract.js";
+import "./App.css";
 
 function App() {
+  const [imagePath, setImagePath] = useState("");
+  const [text, setText] = useState("");
+  const [confidence, setConfidence] = useState("");
+
+  const handleChange = (event) => {
+    setImagePath(URL.createObjectURL(event.target.files[0]));
+  };
+
+  const handleClick = () => {
+    setText("Converting...");
+    Tesseract.recognize(imagePath, "eng", {
+      logger: (m) => console.log(m),
+    })
+      .catch((err) => {
+        console.error(err);
+      })
+      .then((result) => {
+        // Get Confidence score
+        setConfidence(result.data.confidence);
+
+        setText(result.data.text);
+        console.log(result.data);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="App-main">
+        <h3>Extract image from text</h3>
+        {imagePath ? (
+          <img src={imagePath} className="App-image" alt="logo" />
+        ) : (
+          <input type="file" onChange={handleChange} />
+        )}
+
+        {text && <h3>Extracted text</h3>}
+        <div className="text-box">
+          <p> {text} </p>
+          {confidence && <p>Extraction accuracy: {confidence}%</p>}
+        </div>
+
+        {imagePath && !text ? (
+          <button onClick={handleClick} style={{ height: 50 }}>
+            {" "}
+            convert to text
+          </button>
+        ) : null}
+      </main>
     </div>
   );
 }
